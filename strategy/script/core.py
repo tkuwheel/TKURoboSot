4622e9f71672a624f69e7a8379934cc22bb90596
“DamnTT” <thomas19980311@gmail.com>
2019-04-10 19:24:33 +0800

    finish

--- strategy/script/core.py
+++ strategy/script/core.py
@@ -1,62 +1,129 @@
 #!/usr/bin/env python3
 import rospy
-import sys, getopt
+import sys
+import math
 from statemachine import StateMachine, State
 from robot.robot import Robot
 from std_msgs.msg import String
-from my_print import log
+from my_sys import log, SysCheck
+from methods.chase import strategy
+from methods.attack import strategy as attack
+
 
 class SoccerMachine(StateMachine):
   wait    = State('Wait', initial=True)
-  forward = State('Forward')
-  back    = State('Back')
+  chase   = State('Chase')
+  attack  = State('Attack')
+ 
 
-  enter = wait.to(forward)
-  go_forward = back.to(forward)
-  go_back    = forward.to(back)
+  enter = wait.to(chase) | attack.to(chase)
+  stop = chase.to(wait) | attack.to(wait)
+  assault = chase.to(attack)
+  
 
-  def on_enter(self):
+
+  def on_enter_active(self):
     log("I'm running~")
-  def on_go_forward(self):
-    log("I'm running~")
-  def on_go_back(self):
-    log("I'm going back.")
+
+  def on_enter_wait(self):
+    log("I'm stopping.")
+
+  def on_enter_attack(self):
+    log("I'm attacking.")
 
 class Core(Robot):
   sm = SoccerMachine()
-  def __init__(self, robot_num, sim = False):
+  def __init__(self, robot_num, sim = False): #在最一開始創建函式時成立
     self.sim = sim
-    super(Core, self).__init__(robot_num)
-  
+    super(Core, self).__init__(robot_num, sim)
+
   def Brain(self):
     obj = self.GetObjectInfo()
-    if obj['ball']['dis'] is None:
-      log("NONE")
+    if obj['ball']['dis'] == 0:
+      log("NONE")  
     else:
-      self.RobotCtrl(50, 0, 0)
-      log(self.sm.current_state)
+      if self.sm.is_wait :
+        if obj['ball']['dis'] >= 36 :
+          log(self.sm.current_state)
+          print("Ball: {}\tCyan Goal: {}\tMagenta Goal: {}".format(obj['ball'], obj['cyan_goal'], obj['magenta_goal']))
+          self.sm.enter()
+        if obj['ball']['dis'] <= 36 and abs(obj['magenta_goal']['dis'] <= 55) :
+          log("NONE")    
+          log(self.sm.current_state)
+          print("Ball: {}\tCyan Goal: {}\tMagenta Goal: {}".format(obj['ball'], obj['cyan_goal'], obj['magenta_goal']))
+          sys.exit()
+
+      elif self.sm.is_chase :
+        if obj['ball']['dis'] <= 50 and abs(obj['ball']['ang']) <= 20:
+            self.ballhandle()
+            self.sm.assault()
+        else:
+          
+          ro = strategy(self,obj)
+          log(self.sm.current_state)
+          self.pubNubotCtrl(ro['v_x'], ro['v_y'], ro['v_yaw'])
+          print("Ball: {}\tCyan Goal: {}\tMagenta Goal: {}".format(obj['ball'], obj['cyan_goal'], obj['magenta_goal']))
+          if obj['ball']['dis'] <= 36 and abs(obj['magenta_goal']['dis'] <= 55) :  
+            self.sm.stop()
+      elif self.sm.is_attack :
+        if obj['ball']['dis'] > 50 or abs(obj['ball']['ang']) > 20 :
+          self.sm.enter()
+        ro = attack(self,obj)
+        log(self.sm.current_state)
+        self.pubNubotCtrl(ro['v_x'], ro['v_y'], ro['v_yaw'])
+        print("Ball: {}\tCyan Goal: {}\tMagenta Goal: {}".format(obj['ball'], obj['cyan_goal'], obj['magenta_goal']))
+  def pubNubotCtrl(self, x, y, yaw):
+        angle = yaw
+        velocity = math.hypot(x, y)
+        if x != 0:
+            alpha = math.degrees(math.atan2(y, x))
+        else:
+            alpha = 0
+        dis_max = 2
+        dis_min = 0.3
+        velocity_max = 70
+        velocity_min = 50
+        angular_velocity_max = 2
+        angular_velocity_min = 0.5
+        angle_max = 144
+        angle_min = 20
+        angle_out = angle
+        if velocity == 0:
+            pass
+        elif velocity > dis_max:
+            velocity = velocity_max
+        elif velocity < dis_min:
+            velocity = velocity_min
+        else:
+            velocity = (velocity_max - velocity_min) * (math.cos((((velocity - dis_min) / (dis_max-dis_min) - 1) * math.pi)) + 1 )/ 2 + velocity_min
+        if angle == 0:
+            pass
+        elif abs(angle) > angle_max:
+            angle_out = angular_velocity_max
+        elif abs(angle) < angle_min:
+            angle_out = angular_velocity_min
+        else:
+            angle_out = (angular_velocity_max - angular_velocity_min) * (math.cos((((angle - angle_min) / (angle_max-angle_min) - 1) * math.pi)) + 1 )/ 2 + angular_velocity_min
+        if angle < 0:
+            angle_out = -angle_out
+        x = velocity * math.cos(math.radians(alpha))
+        y = velocity * math.sin(math.radians(alpha))
+        yaw = angle_out
+        self.RobotCtrl(x, y, yaw)
+          
+            
+       
 
 def main(argv):
   rospy.init_node('core', anonymous=True)
   rate = rospy.Rate(50)
 
-  try:
-    opts, args = getopt.getopt(argv,"hs",["help", "sim"])
-  except getopt.GetoptError:
-    log("[ERROR] Append argument --sim to start with simulative mode.", True)
-    sys.exit(2)
-
-  if len(argv) < 3:
-    log("Native Mode", rosout = True)
+  if SysCheck(argv) == "Native Mode":
+    log("Start Native")
     robot = Core(1)
-  else:
-    for opt, arg in opts:
-      if opt in ("-h", "--help"):
-        log("Append argument --sim to start with simulative mode.")
-        sys.exit()
-      elif opt in ("-s", "--sim"):
-        log("Simulative Mode", True)
-        robot = Core(1, True)
+  elif SysCheck(argv) == "Simulative Mode":
+    log("Start Sim")
+    robot = Core(1, True)
 
   while not rospy.is_shutdown():
     robot.Brain()
@@ -64,6 +131,7 @@ def main(argv):
 
 if __name__ == '__main__':
   try:
+    
     main(sys.argv[1:])
   except rospy.ROSInterruptException:
     pass
--- strategy/script/robot/robot.py
+++ strategy/script/robot/robot.py
@@ -2,17 +2,20 @@ import rospy
 import math
 from nubot_common.msg import OminiVisionInfo
 from nubot_common.msg import VelCmd
+from nubot_common.srv import Shoot
+from nubot_common.srv import BallHandle
 from transfer.msg import PPoint
 
 SIM_VISION_TOPIC = "nubot{}/omnivision/OmniVisionInfo"
 SIM_CMDVEL_TOPIC = "nubot{}/nubotcontrol/velcmd"
 
+
 class Robot(object):
 
   __robot_info = {'location' : None}
-  __object_info = {'ball':{'dis' : None, 'ang' : None},
-                   'cyan_goal':{'dis' : None, 'ang' : None},
-                   'magenta_goal':{'dis' : None, 'ang' : None}}
+  __object_info = {'ball':{'dis' : 0, 'ang' : 0},
+                   'cyan_goal':{'dis' : 0, 'ang' : 0},
+                   'magenta_goal':{'dis' : 0, 'ang' : 0}}
 
   def ShowRobotInfo(self):
     print("Robot informations: {}".format(self.__robot_info))
@@ -21,11 +24,11 @@ class Robot(object):
   def __init__(self, robot_num, sim = False):
     self.robot_number = robot_num
     if not sim :
-      self._Subscriber(SIM_VISION_TOPIC.format(self.robot_number))
-      self._Publisher(SIM_CMDVEL_TOPIC.format(self.robot_number))
-    else:
       self._Subscriber("")
       self._Publisher("")
+    else:
+      self._Subscriber(SIM_VISION_TOPIC.format(self.robot_number))
+      self._Publisher(SIM_CMDVEL_TOPIC.format(self.robot_number))
 
   def _Subscriber(self, topic):
     rospy.Subscriber(topic.format(self.robot_number), \
@@ -59,3 +62,23 @@ class Robot(object):
 
   def GetObjectInfo(self):
     return self.__object_info
+
+  def shoot(self,x,y) :
+    rospy.wait_for_service('nubot1/Shoot')
+    try:
+      Shoot_client = rospy.ServiceProxy('nubot1/Shoot',Shoot)
+      resp1 = Shoot_client(x,y)
+      return resp1
+    except(rospy.ServiceException):
+      print("Service call failed")
+
+  
+  def ballhandle(self):
+    rospy.wait_for_service('nubot1/Ballhandle')
+    try:
+      Ballhandle_client = rospy.ServiceProxy('nubot1/Ballhandle',BallHandle)
+      resp1 = Ballhandle_client(1)
+      return resp1
+    except(rospy.ServiceException):
+      print("Service call failed") 
+  
