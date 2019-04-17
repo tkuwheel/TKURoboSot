11e73b38991f03b4cc3a500223f7e206b3344282
NEET <zxc455233@gmail.com>
2019-04-17 22:21:03 +0800

    First Simple Strategy version
    Combine Class 'Robot' 'StateMachine' into Core
    Make Methods 'chase' and 'attack' to be Class

--- strategy/script/core.py
+++ strategy/script/core.py
@@ -3,53 +3,63 @@ import rospy
 import sys
 #sys.path.insert(0, "/home/superwheel/RoboSot/devel/lib/python2.7/dist-packages")
 import math
+import time
 from statemachine import StateMachine, State
 from robot.robot import Robot
 from std_msgs.msg import String
 from my_sys import log, SysCheck, logInOne
 from methods.chase import Chase
-from methods.attack import strategy as attack
+from methods.attack import Attack
 from dynamic_reconfigure.server import Server
 from strategy.cfg import GameStateConfig
 
 class Core(Robot, StateMachine):
-  idle   = State('Idle', initial = True)
-  chase  = State('Chase')
-  attack = State('Attack')
-
-  toChase  = idle.to(chase) | attack.to(chase)
-  toIdle   = chase.to(idle) | attack.to(idle)
-  toAttack = chase.to(attack)
-
-  def on_toChase(self):
-    log("To Chase")
-  
-  def on_toIdle(self):
-    log("To Idle")
-
-  def on_toAttack(self):
-    log("To Attack")
-
   def __init__(self, robot_num, sim = False):
     self.CC  = Chase()
+    self.AC  = Attack()
     self.sim = sim
     super(Core, self).__init__(robot_num, sim)
     StateMachine.__init__(self)
 
+  idle   = State('Idle', initial = True)
+  chase  = State('Chase')
+  attack = State('Attack')
+
+  toChase  = idle.to(chase) | attack.to(chase) | chase.to(chase)
+  toIdle   = chase.to(idle) | attack.to(idle)
+  toAttack = chase.to(attack)
+
+  def on_toChase(self, t):
+    o = self.CC.ClassicRounding(t['magenta_goal']['ang'],\
+                                t['ball']['dis'],\
+                                t['ball']['ang'])
+    self.RobotCtrl(o['v_x'], o['v_y'], o['v_yaw'])
+
+  def on_toIdle(self):
+    log("To Idle")
+
+  def on_toAttack(self, t):
+    o = self.AC.ClassicAttacking(t['magenta_goal']['dis'], t['magenta_goal']['ang'])
+    self.RobotCtrl(o['v_x'], o['v_y'], o['v_yaw'])
+
 class Strategy(object):
   def __init__(self):
-    pass
+    gains = rospy.get_param("core")
+    self.game_start = gains['game_start']
+    self.game_state = gains['game_state']
+    self.side       = gains['side']
 
   def Callback(self, config, level):
-    rospy.loginfo(config)
+    self.game_start = config['game_start']
+    self.game_state = config['game_state']
+    self.side       = config['side']
     return config
 
   def main(self, argv):
     rospy.init_node('core', anonymous=True)
-    rate = rospy.Rate(10)
+    rate = rospy.Rate(30)
 
-    srv = Server(GameStateConfig, self.Callback)
-    print(srv)
+    dsrv = Server(GameStateConfig, self.Callback)
 
     if SysCheck(argv) == "Native Mode":
       log("Start Native")
@@ -59,16 +69,34 @@ class Strategy(object):
       robot = Core(1, True)
 
     while not rospy.is_shutdown():
-      gains = rospy.get_param("game_state_server", False)
       targets = robot.GetObjectInfo()
 
       while targets is not None:
         targets = robot.GetObjectInfo()
-        if robot.is_idle:
-          print(robot.current_state)
-          exit()
-        rospy.spinOnce()
-        rate.sleep()
+
+        if not robot.is_idle and not self.game_start:
+          # stay idle
+          robot.toIdle()
+        elif robot.is_idle and self.game_start:
+          # go chase
+          robot.toChase(targets)
+        elif robot.is_chase and targets['ball']['dis'] >= 37:
+          # keep chase
+          log("keep{}".format(targets['ball']['dis']))
+          robot.toChase(targets)
+        elif robot.is_chase and targets['ball']['ang'] < 20:
+          # go attack
+          robot.toAttack(targets)
+        elif robot.is_attack and targets['ball']['dis'] > 30:
+          # back chase
+          robot.toChase(targets)
+
+        if rospy.is_shutdown():
+          log('shutdown')
+          break
+
+    rospy.spin()
+    rate.sleep()
 
 if __name__ == '__main__':
   try:
--- strategy/script/methods/chase.py
+++ strategy/script/methods/chase.py
@@ -4,9 +4,10 @@ import math
 
 class Chase(object):
   __pub_info = {'v_x':None,'v_y':None,'v_yaw':None}
+  __goal = None
 
-  def ClassicRounding(self, obj):
-    alpha = math.radians(obj['ball']['ang'] - obj['magenta_goal']['ang'])
+  def ClassicRounding(self, goal_ang, ball_dis, ball_ang):
+    alpha = math.radians(ball_ang - goal_ang)
     beta = 0.7
     if abs(alpha) > beta:
       if alpha > 0 :
@@ -16,10 +17,29 @@ class Chase(object):
     else:
       pass
 
-    br_x = obj['ball']['dis'] * math.cos(math.radians(obj['ball']['ang']))
-    br_y = obj['ball']['dis'] * math.sin(math.radians(obj['ball']['ang']))
+    br_x = ball_dis * math.cos(math.radians(ball_ang))
+    br_y = ball_dis * math.sin(math.radians(ball_ang))
     self.__pub_info['v_x']   = br_x * math.cos(alpha) - br_y * math.sin(alpha)
     self.__pub_info['v_y']   = br_x * math.sin(alpha) + br_y * math.cos(alpha)
-    self.__pub_info['v_yaw'] = obj['magenta_goal']['ang']
+    self.__pub_info['v_yaw'] = goal_ang
     
     return self.__pub_info
+
+#  def ClassicRounding(self, goal_ang=None, ball_dis=None, ball_ang=None, obj):
+#    alpha = math.radians(obj['ball']['ang'] - obj['magenta_goal']['ang'])
+#    beta = 0.7
+#    if abs(alpha) > beta:
+#      if alpha > 0 :
+#        alpha = beta
+#      else:
+#        alpha = -beta
+#    else:
+#      pass
+#
+#    br_x = obj['ball']['dis'] * math.cos(math.radians(obj['ball']['ang']))
+#    br_y = obj['ball']['dis'] * math.sin(math.radians(obj['ball']['ang']))
+#    self.__pub_info['v_x']   = br_x * math.cos(alpha) - br_y * math.sin(alpha)
+#    self.__pub_info['v_y']   = br_x * math.sin(alpha) + br_y * math.cos(alpha)
+#    self.__pub_info['v_yaw'] = obj['magenta_goal']['ang']
+#    
+#    return self.__pub_info
