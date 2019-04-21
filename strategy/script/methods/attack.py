7857149868aa40ef09278590349ce9357a67c921
“DamnTT” <thomas19980311@gmail.com>
2019-04-21 12:50:27 +0800

    註解東西

--- strategy/script/methods/attack.py
+++ strategy/script/methods/attack.py
@@ -2,6 +2,29 @@
 import rospy
 import math
 
+'''def strategy(self,obj):
+  __pub_info = {'v_x':0.0,'v_y':0.0,'v_yaw':0.0} 
+  alpha = math.radians(obj['ball']['ang'] - obj['magenta_goal']['ang'])
+  beta = 0.7
+  if abs(alpha) > beta:
+    angle_type = 'beta'
+    if alpha > 0 :
+      alpha = beta
+    else:
+      alpha = -beta
+  else:
+    angle_type = 'alpha'
+  
+ 
+
+  __pub_info['v_x'] = obj['magenta_goal']['dis'] * math.cos(math.radians(obj['magenta_goal']['ang']))
+  __pub_info['v_y'] = obj['magenta_goal']['dis'] * math.sin(math.radians(obj['magenta_goal']['ang']))
+  __pub_info['v_yaw'] = obj['magenta_goal']['ang']
+  strategy_type = 'attack'
+
+
+  
+  return __pub_info'''
 class Attack(object):
   __pub_info = {'v_x':None,'v_y':None,'v_yaw':None} 
 
--- strategy/script/methods/chase.py
+++ strategy/script/methods/chase.py
@@ -2,6 +2,20 @@
 import rospy
 import math
 
+'''<<<<<<< HEAD
+def strategy(self,obj):
+  __pub_info = {'v_x':0.0,'v_y':0.0,'v_yaw':0.0}              
+  
+  br_x = obj['ball']['dis'] * math.cos(math.radians(obj['ball']['ang']))
+  br_y = obj['ball']['dis'] * math.sin(math.radians(obj['ball']['ang']))
+  __pub_info['v_x'] = br_x #* math.cos(alpha) - br_y * math.sin(alpha)
+  __pub_info['v_y']= br_y #* math.sin(alpha) + br_y * math.cos(alpha)
+
+  __pub_info['v_yaw'] = obj['magenta_goal']['ang']
+  strategy_type = 'chase'
+  
+  return __pub_info
+======='''
 class Chase(object):
   __pub_info = {'v_x':None,'v_y':None,'v_yaw':None}
   __goal = None
@@ -43,3 +57,4 @@ class Chase(object):
 #    self.__pub_info['v_yaw'] = obj['magenta_goal']['ang']
 #    
 #    return self.__pub_info
+>>>>>>> tkurobot/master
