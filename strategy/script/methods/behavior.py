1eee68b670b3e8390df66f2e002f5fe432804736
DamnTT <thomas19980311@gmail.com>
2019-06-23 22:46:55 +0800

    run the point suceesfully

--- strategy/script/methods/behavior.py
+++ strategy/script/methods/behavior.py
@@ -15,9 +15,11 @@ class Behavior(Robot):
    
     v_x   = tx - robot_info['location']['x']
     v_y   = ty - robot_info['location']['y']
+    # o_x, o_y = self._Rotate(v_x, v_y, robot_info['location']['yaw']*-1)
+   
     v_yaw = tyaw - robot_info['location']['yaw']
     
-    if sqrt(v_x**2 + v_y**2) < 50:
+    if math.sqrt(v_x**2 + v_y**2) < 50:
       get_the_point = 1
 
     return v_x, v_y, v_yaw, get_the_point
