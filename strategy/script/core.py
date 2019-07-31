9ad6c48a8a40a0d58bb8dbb719dc25b004c537d2
DamnTT <thomas19980311@gmail.com>
2019-08-01 02:06:40 +0800

    new rqt_web v.2 add connect sign

--- strategy/script/core.py
+++ strategy/script/core.py
@@ -229,11 +229,11 @@ class Strategy(object):
   def RunStatePoint(self):
     print("run point")
     if self.robot.run_point == "ball_hand":
-      if self.robot.toPoint:
+      if self.robot.toPoint():
         self.dclient.update_configuration({"run_point": "none"})
         self.ToMovement()
     elif self.robot.run_point == "empty_hand":
-      if self.robot.toPoint:
+      if self.robot.toPoint():
         self.dclient.update_configuration({"run_point": "none"})
         self.ToChase()
 
@@ -381,11 +381,15 @@ class Strategy(object):
           self.ToAttack()
 
         ## Run point
-        if self.robot.is_point:
-          if not self.robot.CheckBallHandle():
-            self.ToChase()
+       if self.robot.is_point:
+          if point == "ball_hand":
+            if self.robot.CheckBallHandle():
+              self.RunStatePoint()
+            else:
+              self.ToChase(role)
           else:
             self.RunStatePoint()
+
 
       if rospy.is_shutdown():
         log('shutdown')
